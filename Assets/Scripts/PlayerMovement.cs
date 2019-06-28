0440d1020b5a8fe115d6bf7a1d49b399b7fee821
Mirari <zf4@illinois.edu>
2019-06-27 23:15:22 -0700

    [level] level design first draft

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody2D rigidBody;
+    Vector2 change;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector2.zero;
+        change.x = Input.GetAxis("Horizontal");
+        change.y = Input.GetAxis("Vertical");
+        UpdateAnimationAndMove();
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector2.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        rigidBody.MovePosition(rigidBody.position + change * speed * Time.deltaTime);
+    }
+}
