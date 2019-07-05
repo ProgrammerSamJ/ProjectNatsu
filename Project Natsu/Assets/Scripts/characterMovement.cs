83d00002acfa576c4d2914358c64db8c8d81f4ec
Ziting Wang <wangz26@rpi.edu>
2019-07-05 13:46:28 +0800

    Level Design 6-10 Ver.1
    Add level design 6-10 Ver.1

---
+++ Project Natsu/Assets/Scripts/characterMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterMovement : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+
+    public int maxHealth = 5;
+    public float speed = 3.0f;
+    int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        //Debug.Log(horizontal);
+        //Debug.Log(vertical);
+
+        Vector2 position = rigidbody2D.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        rigidbody2D.MovePosition(position);
+    }
+
+    void changeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+}
