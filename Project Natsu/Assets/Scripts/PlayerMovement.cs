9191a14c4043f6934113c4fb6adc98513fb53f06
Ziting Wang <wangz26@rpi.edu>
2019-07-11 00:09:45 +0800

    Added healthbar and melee enemy

---
+++ Project Natsu/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float startHaelth = 100;
+    private float currHealth;
+
+    public Image healthBar;
+
+    Rigidbody2D rigidBody;
+    Vector2 change;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        currHealth = startHaelth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector2.zero;
+        change.x = Input.GetAxis("Horizontal");
+        change.y = Input.GetAxis("Vertical");
+        UpdateAnimationAndMove();
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector2.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        rigidBody.MovePosition(rigidBody.position + change * speed * Time.deltaTime);
+    }
+
+    void takeDamage(float amount)
+    {
+        currHealth -= amount;
+
+        healthBar.fillAmount = currHealth / startHaelth;
+    }
+}
---
+++ Project Natsu/Assets/Scripts/meleeEnemyMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class meleeEnemyMovement : MonoBehaviour
+{
+    public float speed;
+    public float startHaelth = 10;
+    private float currHealth;
+
+    public Image healthBar;
+
+    Rigidbody2D rigidBody;
+    Vector2 change;
+    Animator animator;
+
+    //facing
+    public GameObject enemyGraphic;
+
+    public float radius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        currHealth = startHaelth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector2.zero;
+        change.x = Input.GetAxis("Horizontal");
+        change.y = Input.GetAxis("Vertical");
+        UpdateAnimationAndMove();
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector2.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        rigidBody.MovePosition(rigidBody.position + change * speed * Time.deltaTime);
+    }
+}
