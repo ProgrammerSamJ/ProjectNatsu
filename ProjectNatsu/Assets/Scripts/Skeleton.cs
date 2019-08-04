18029afa8be6972d81225bde4df5179ad19393bc
Kai X <kcxu@bu.edu>
2019-08-04 11:04:37 +0800

    new commit 8/4

---
+++ ProjectNatsu/Assets/Scripts/Skeleton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Skeleton : Enemy
+{
+
+
+    // Start is called before the first frame update
+    public Transform target;
+    public float chaseRadius;
+    public float attackRadius;
+    public Transform homePosition;
+    private Animator animator;
+
+    void Start()
+    {
+          animator = GetComponent<Animator>();
+          animator.SetBool("Move", false);
+          animator.SetBool("Attack", false);
+          target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          CheckDistance();
+    }
+
+    void CheckDistance(){
+          if (Vector3.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > attackRadius){
+                animator.SetBool("Move", true);
+                transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+          }
+          else if(Vector3.Distance(target.position, transform.position) <= attackRadius){
+                animator.SetBool("Attack", true);
+          }
+          else{
+                animator.SetBool("Move", false);
+                animator.SetBool("Attack", false);
+          }
+    }
+}
